58f5f797f13634d99aafd6c41d5fa4ee232de7ad
james <jbolanosdiaz@gmail.com>
2024-11-23 20:25:38 -0600

    corrected file svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -2,8 +2,6 @@ import vercel from '@sveltejs/adapter-vercel';
 
 export default {
     kit: {
-        adapter: vercel({
-            runtime: 'nodejs18.x', // Specify Node.js version
-        })
+        adapter: vercel()
     }
 };
